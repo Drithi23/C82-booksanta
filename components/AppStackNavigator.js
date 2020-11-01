f4b9c354b86d3a87084b30bb9c91305c60d22dc9
Drithi23 <59303904+Drithi23@users.noreply.github.com>
2020-11-01 15:22:18 +0530

    Add files via upload

---
+++ components/AppStackNavigator.js
@@ -0,0 +1,22 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import BookDonateScreen from '../screens/BookDonateScreen';
+import RecieverDetailScreen from '../screens/RecieverDetailsScreen';
+
+export const AppStackNavigator = createStackNavigator({
+    BookDonateList: {
+      screen: BookDonateScreen,
+      navigationOptions: {
+        headerShown: false,
+      }
+    },
+    RecieverDetails: {
+      screen: RecieverDetailScreen,
+      navigationOptions: {
+        headerShown: false,
+      }
+    }
+  },
+  {
+    initialRouteName: 'BookDonateList'
+  },
+)
